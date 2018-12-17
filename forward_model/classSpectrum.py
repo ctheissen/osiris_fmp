276e97362b7d26f330772191ceaecde04e114864
Chris Theissen <ctheissen@gmail.com>
2018-12-17 10:21:27 -0800

    Fixed the spectrum class to not need nirspec order

--- forward_model/classSpectrum.py
+++ forward_model/classSpectrum.py
@@ -51,7 +51,7 @@ class Spectrum():
 	"""
 	def __init__(self, **kwargs):
 		self.name  = kwargs.get('name')
-		self.order = kwargs.get('order')
+		self.order = kwargs.get('order', 33)
 		self.path  = kwargs.get('path')
 		self.applymask = kwargs.get('applymask',False)
 		#self.manaulmask = kwargs('manaulmask', False)
